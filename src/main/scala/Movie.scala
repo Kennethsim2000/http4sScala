e831b326fb8db5d1b044925c5b1511a2ae47868e
kenneth <kenneth12345645@gmail.com>
2025-07-08 13:08:44 +0800

    Add in optional validating queryparam decoder for year and http4s entity decoder for director post

--- src/main/scala/Movie.scala
+++ src/main/scala/Movie.scala
@@ -1,15 +1,18 @@
 import cats.*
 import cats.effect.*
-import cats.syntax.either.*
-import org.http4s.{HttpApp, HttpRoutes, ParseFailure, QueryParamDecoder}
+import cats.implicits.* // required for req.as[Director]
+import org.http4s.*
 import org.http4s.dsl.Http4sDsl
-import org.http4s.dsl.impl.{OptionalQueryParamDecoderMatcher, QueryParamDecoderMatcher}
+import org.http4s.dsl.impl.{OptionalValidatingQueryParamDecoderMatcher, QueryParamDecoderMatcher}
 import org.http4s.syntax.kleisli.*
-import io.circe.syntax.*
-import io.circe.generic.auto.*
+import io.circe.syntax.* // bring into scope asJson
+import io.circe.generic.auto.* //Automatically generates Encoder and Decoder instances for your case classes.
 import org.http4s.circe.*
+import org.http4s.headers.`Content-Encoding`
+import org.typelevel.ci.CIString
 
 import java.time.Year
+import scala.collection.mutable
 import scala.util.Try
 
 object Movie extends IOApp {
@@ -22,7 +25,14 @@ object Movie extends IOApp {
 
     object DirectorQueryParamMatcher extends QueryParamDecoderMatcher[String]("director")
     //The QueryParamDecoderMatcher requires the name of the parameter and its type
-    object YearQueryParamMatcher extends OptionalQueryParamDecoderMatcher[Year]("year")
+    object YearQueryParamMatcher extends OptionalValidatingQueryParamDecoderMatcher[Year]("year")
+    //this returns a Option[ValidatedNel[ParseFailure, A]]
+    //In Cats, Validated is a data type that represents a value that is either:
+    //Valid[A] – success case, holds a value of type A
+    //Invalid[E] – failure case, holds an error (often a collection like NonEmptyList[E])
+    //
+    //This is similar to Either, but unlike Either, Validated is often used for accumulating errors, especially in form
+    // or query param validation.
 
     //create an implicit queryParamDecoder that can be used by our YearQueryParamDecoderMatcher
     implicit val yearQueryParamDecoder: QueryParamDecoder[Year] = QueryParamDecoder[Int]
@@ -36,13 +46,21 @@ object Movie extends IOApp {
     //emap returns an Either, where the leftMap is a ParseFailure
     //ParseFailure is a type of the http4s library indicating an error parsing an HTTP Message
 
+
     //GET / movies ? director = Zack % 20 Snyder & year = 2021
     //GET /movies/aa4f0f9c-c703-4f21-8c05-6a0c8f2052f0/actors
     def movieRoutes[F[_]: Monad]: HttpRoutes[F] = {
         val dsl = Http4sDsl[F]
         import dsl._
         HttpRoutes.of[F] {
-            case GET -> Root / "Movies" :? DirectorQueryParamMatcher(director) +& YearQueryParamMatcher(year) => ???
+            case GET -> Root / "Movies" :? DirectorQueryParamMatcher(director) +& YearQueryParamMatcher(maybeYear) =>
+                maybeYear match {
+                    case Some(year) => year.fold(
+                        _ => BadRequest("The given year is not valid"), //invalid
+                        year => ??? // valid
+                    )
+                    case None => ???
+                } //The method .fold on Validated[E, A] lets you handle both cases (Invalid and Valid) in a functional way.
             case GET -> Root/ "Movies"/ UUIDVar(movieId) / "actors" => ???
             //UUIDVar is a route extractor used to match and convert path segments into UUID objects.
         }
@@ -64,26 +82,43 @@ object Movie extends IOApp {
     }
 
     // mutable map mapping actor to director
-    val directorMap:Map[Actor, Director] = Map("Zack Snyder" -> Director("Zack", "Snyder"));
+    val directorMap:mutable.Map[Actor, Director] = mutable.Map("Zack Snyder" -> Director("Zack", "Snyder"));
 
-    def directorRoutes[F[_]: Monad]: HttpRoutes[F] = {
+
+    //Concurrent[F] is a type class from Cats Effect that describes effect types that support concurrency and
+    // cancelation, like IO, Task, ZIO
+    //jsonOf[F, A] is the http4s + Circe integration that creates an EntityDecoder[F, A]. It’s used to parse JSON from
+    // an HTTP request body into a Scala type
+    def directorRoutes[F[_]: Concurrent]: HttpRoutes[F] = {
         val dsl = Http4sDsl[F]
         import dsl._
+        implicit val directorDecoder: EntityDecoder[F, Director] = jsonOf[F, Director]
+
+        // requires a Concurrent constraint instead of monad
+        //Because reading a request body is asynchronous and cancelable, it requires Concurrent[F], not just Monad[F]
         HttpRoutes.of[F] {
             case GET -> Root / "directors"/ DirectorVar(director) =>
                 directorMap.get(director.toString) match {
-                    case Some(foundDirector) => Ok(foundDirector.asJson)
+                    case Some(foundDirector) => Ok(foundDirector.asJson, Header.Raw(CIString("my-custom-Header"), "value"))
                     case None => NotFound(s"No director called $director found")
                 }
+            case req@POST -> Root/ "directors" =>
+                for {
+                    director <- req.as[Director] // need to import cats.implicits._
+                    _ = directorMap.put(director.toString, director)
+                    res <- Ok.headers(`Content-Encoding`(ContentCoding.gzip))
+                        .map(_.addCookie(ResponseCookie("My-Cookie", "value")))
+                } yield res
         }
     }
+
     //composing routes
-    def allRoutes[F[_]: Monad] :HttpRoutes[F] = {
+    def allRoutes[F[_]: Concurrent] :HttpRoutes[F] = {
         import cats.syntax.semigroupk._
         movieRoutes[F] <+> directorRoutes[F]
     }
     //handle if some routes do not match with any of the available routes
-    def allRoutesComplete[F[_]: Monad]: HttpApp[F] = {
+    def allRoutesComplete[F[_]: Concurrent]: HttpApp[F] = {
         allRoutes.orNotFound
     }
     /* As we can see, the method returns an instance of the type HttpApp[F], which is a type alias for
@@ -106,3 +141,9 @@ object Movie extends IOApp {
 *Nevertheless, not all the Request will find a route to a Response. So, we need to take into consideration this fact,
 * defining a route as a function of type Request => F[Option[Response]]. Using a monad transformer, we can translate
 * this type in Request => OptionT[F, Response].*/
+
+/*
+* Circe Encoder[A] – knows how to convert a Scala object into JSON
+
+Http4s EntityEncoder[F, A] – knows how to send an A in an HTTP response (e.g. with proper content-type, charset, etc.)
+*  */
